42e235b53012ac452112e0b69f2b9371dc4fb11a
= <=>
2022-04-18 18:51:57 +0900

    ..

--- marvel-app/src/components/EventsSeries.tsx
+++ marvel-app/src/components/EventsSeries.tsx
@@ -1,12 +1,35 @@
 import axios from "axios";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 import { apikey, BASE_URL, GET_EVENTS, hash } from "../api";
 import { ISeries } from "../types_store/SeriesType";
-import { Wrapper } from "./CharacterComics";
+import { LeftArrow, RightArrow, Wrapper } from "./CharacterComics";
 import { SeriesElem, ShowMoreBtn } from "./CharacterSeries";
 
-const LIMIT = 12;
+const LeftArrowBox = styled.div`
+    width: 70px;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    align-items: center;
+`;
+
+const RightArrowBox = styled.div`
+    width: 70px;
+    height: 100%;
+    position: absolute;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    align-items: center;
+`;
+
+const a = { position: 'absolute', left: '0', right: '0', margin: 'auto' };
+
+const LIMIT = 20;
 
 function EventSeries({ id }: { id: string }) {
 
@@ -33,22 +56,51 @@ function EventSeries({ id }: { id: string }) {
 
     return (
         <>
-            <Wrapper>
-                {
-                    series?.data.results.map(seriesElem => {
-                        return (
-                            <motion.span
-                            key={seriesElem.id}
-                            layoutId={seriesElem.id + ''}
-                            >
-                                <SeriesElem
-                                path={seriesElem.thumbnail.path + "/standard_amazing.jpg"}
+            <Wrapper style={{ 
+                position: 'relative',
+                maxWidth: '1950px'
+            }}
+            >
+                <AnimatePresence>
+                    {
+                        series?.data.results.
+                        slice(0, 7).map(seriesElem => {
+                            return (
+                                <motion.span
+                                key={seriesElem.id}
+                                layoutId={seriesElem.id + ''}
                                 >
-                                </SeriesElem>    
-                            </motion.span>
-                        )
-                    })
-                }
+                                    <SeriesElem
+                                    path={seriesElem.thumbnail.path + "/standard_amazing.jpg"}
+                                    >
+                                    </SeriesElem>    
+                                </motion.span>
+                            )
+                        })
+                    }
+                </AnimatePresence>
+                <LeftArrowBox>
+                    <LeftArrow
+                    style={{ 
+                        position: 'absolute', 
+                        left: '0', 
+                        right: '0', 
+                        margin: 'auto'
+                    }}
+                    src={require('../images/arrow.png')}
+                    />
+                </LeftArrowBox>
+                <RightArrowBox>
+                    <RightArrow 
+                    style={{ 
+                        position: 'absolute', 
+                        left: '0', 
+                        right: '0', 
+                        margin: 'auto'
+                    }}
+                    src={require('../images/arrow.png')}
+                    />
+                </RightArrowBox>
             </Wrapper>
             <br></br>
             <br></br>
