a920b82b9f73b15fa79048cf6c5cdfbb3d290edc
jdy8739 <jdy8739@naver.com>
2022-07-02 17:05:13 +0900

    .

--- marvel-app/src/App.tsx
+++ marvel-app/src/App.tsx
@@ -1,15 +1,15 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import Navigation from './components/Navigation';
-import Characters from './routes/characters/Characters';
-import CharactersDetail from './routes/character_detail/CharactersDetail';
-import Comics from './routes/comics/Comics';
-import ComicsDetail from './routes/comics_detail/ComicsDetail';
-import Events from './routes/events/Events';
-import EventsDetail from './routes/events_detail/EventsDetail';
+// import Characters from './routes/characters/Characters';
+// import CharactersDetail from './routes/character_detail/CharactersDetail';
+// import Comics from './routes/comics/Comics';
+// import ComicsDetail from './routes/comics_detail/ComicsDetail';
+// import Events from './routes/events/Events';
+// import EventsDetail from './routes/events_detail/EventsDetail';
 import Home from './routes/Home';
-import Series from './routes/series/Series';
-import SeriesDetail from './routes/series_detail/SeriesDetail';
+// import Series from './routes/series/Series';
+// import SeriesDetail from './routes/series_detail/SeriesDetail';
 import React from 'react';
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
 				<BrowserRouter>
 					<Navigation />
 					<Routes>
-						<Route
+						{/* <Route
 							path="/characters/detail/:id/*"
 							element={<CharactersDetail />}
 						/>
@@ -37,8 +37,8 @@ function App() {
 						<Route
 							path="/events/detail/:id/*"
 							element={<EventsDetail />}
-						/>
-						<Route path="/events/*" element={<Events />} />
+						/> */}
+						{/* <Route path="/events/*" element={<Events />} /> */}
 						<Route path="/*" element={<Home />} />
 					</Routes>
 				</BrowserRouter>
--- marvel-app/src/routes/Home.tsx
+++ marvel-app/src/routes/Home.tsx
@@ -1,58 +1,58 @@
-import styled from 'styled-components';
-import { Helmet } from 'react-helmet';
-import React from 'react';
+import styled from "styled-components";
+import { Helmet } from "react-helmet";
+import React from "react";
 
 const TitleBox = styled.div`
-	position: absolute;
-	h1,
-	h3 {
-		margin: 0;
-	}
-	h1 {
-		font-size: 60px;
-	}
-	h3 {
-		letter-spacing: 11.5px;
-	}
-	p {
-		font-size: 12px;
-	}
-	animation: show 3s;
-	@keyframes show {
-		0% {
-			opacity: 0;
-		}
-		100% {
-			opacity: 1;
-		}
-	}
+    position: absolute;
+    h1,
+    h3 {
+        margin: 0;
+    }
+    h1 {
+        font-size: 60px;
+    }
+    h3 {
+        letter-spacing: 11.5px;
+    }
+    p {
+        font-size: 12px;
+    }
+    animation: show 3s;
+    @keyframes show {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
 `;
 
 function Home() {
-	return (
-		<div
-			style={{
-				position: 'relative',
-				width: '100vw',
-				height: '100vh',
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				textAlign: 'center',
-			}}
-		>
-			<Helmet>
-				<title>Marvel Dictionary</title>
-			</Helmet>
-			<TitleBox>
-				<h1>MARVEL</h1>
-				<h3>DICTIONARY</h3>
-				<br></br>
-				<br></br>
-				<p>made by Do Young Chung</p>
-			</TitleBox>
-		</div>
-	);
+    return (
+        <div
+            style={{
+                position: "relative",
+                width: "100vw",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+            }}
+        >
+            <Helmet>
+                <title>Marvel Dictionary</title>
+            </Helmet>
+            <TitleBox>
+                <h1>MARVEL</h1>
+                <h3>DICTIONARY</h3>
+                <br></br>
+                <br></br>
+                <p>made by Do Young Chung</p>
+            </TitleBox>
+        </div>
+    );
 }
 
 export default Home;
